9192ab65c5f77a8ae90dc2e19d00b5b9b53a1499
siva <sivamani_k@srmap.edu.in>
2024-08-23 20:24:09 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes'; // Import the router
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import Register from './components/Register.jsx';
+import Login from './components/Login';
+import TaskManager from './components/TaskManager';
+import './index.css';
+import Navbar from './components/Navbar.jsx';
+
+function AppRoutes() {
+  let routes = useRoutes([
+    { path: "/register", element: <Register /> },
+    { path: "/login", element: <Login /> },
+    { path: "/tasks", element: <TaskManager /> },
+  ]);
+  return routes;
+}
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <RouterProvider router={router} />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100 p-6">
+        <Navbar />
+        <AppRoutes />
+      </div>
+    </Router>
   );
 }
 
