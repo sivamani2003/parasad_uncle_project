39a1b9084ac517477b0ed3d36b11f423ceb297db
siva <sivamani_k@srmap.edu.in>
2024-08-23 10:05:37 +0530

    update

--- frontend/src/components/TaskManager.jsx
+++ frontend/src/components/TaskManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { generateInvoicePDF } from './invoiceUtils'; // Import the function
+import { generateInvoicePDF } from './invoiceUtils';
 
 function TaskManager() {
   const [tasks, setTasks] = useState([]);
